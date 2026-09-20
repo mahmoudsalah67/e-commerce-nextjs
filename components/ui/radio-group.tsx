94d550d94b9ab906bb1edc3e848129805c168d6c
mahmoudsalah67 <mahmoudsalah@example.com>
2026-09-20 15:29:21 +0300

    '

--- components/ui/radio-group.tsx
+++ components/ui/radio-group.tsx
@@ -2,9 +2,9 @@
 
 import { Radio as RadioPrimitive } from "@base-ui/react/radio"
 import { RadioGroup as RadioGroupPrimitive } from "@base-ui/react/radio-group"
+import { cn } from "../../lib/utils"
 
-import { cn } from "@/lib/utils"
-
+ 
 function RadioGroup({ className, ...props }: RadioGroupPrimitive.Props) {
   return (
     <RadioGroupPrimitive
