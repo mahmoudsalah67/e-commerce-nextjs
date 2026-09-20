b59bfd270ceddeb52958d62b4a56ebb420040b8c
mahmoudsalah67 <mahmoudsalah@example.com>
2026-09-20 17:55:03 +0300

    '

--- src/app/_Components/Footer/Footer.tsx
+++ src/app/_Components/Footer/Footer.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="bg-gray-800 text-white py-4 text-center mt-20">Footer</div>
+    <div className="bg-gray-800 text-white py-4 text-center mt-20">Copy Right 2018 © By Fresh Cart All Rights Reserved</div>
   )
 }
