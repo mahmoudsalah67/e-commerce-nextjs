e300a0d77fcaa09ab782cf6f8c42ac3691403d10
mahmoudsalah67 <mahmoudsalah@example.com>
2026-09-24 14:47:55 +0300

    new code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default async function Home() {
     }>
       <Categoriesslider />
     </Suspense>
-      <div className="mx-auto mt-19 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 px-30">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {alldata?.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
